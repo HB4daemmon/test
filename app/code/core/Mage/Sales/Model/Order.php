5289dc7bb222e0992c8a0e7c2a1da7f14cbdfec5
DaemonWang <hb4daemon@163.com>
2016-06-19 19:31:21 +0800

    Finish backend

--- app/code/core/Mage/Sales/Model/Order.php
+++ app/code/core/Mage/Sales/Model/Order.php
@@ -2117,7 +2117,7 @@ class Mage_Sales_Model_Order extends Mage_Sales_Model_Abstract
         $order['substitute'] = number_format($substitute,2);
         $order['tax'] = number_format($tax,2);
 
-        $order['total'] = number_format($this->getData('grand_total') - $this->getData('original_tax') + $out_of_stock + $substitute + $tax,2);
+        $order['total'] = number_format($this->getData('grand_total') - $this->getData('tax_amount') + $out_of_stock + $substitute + $tax,2);
         $order['original_grand_total'] = number_format($this->getData('grand_total'),2);
         $order['change'] = number_format($order['total'] - $order['original_grand_total'],2);
 
